e2c5617886eb07052c8dc9a962c237f51a237784
Mohd-Shaffan <muhdshaffan84@gmail.com>
2026-08-31 01:11:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.4%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int find(int n,int k ){
+        if(n==1){
+            return 0;
+        }        
+            
+
+
+        int idx= find(n-1,k);
+        idx=(idx+k)%n;
+        return idx;
+}
+    int findTheWinner(int n, int k) {
+        return find(n,k) +1;
+        
+    }
+};
