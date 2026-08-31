c80347d5da578c1ac2bfd1df55a0af7d40631c9e
Mohd-Shaffan <muhdshaffan84@gmail.com>
2026-09-01 01:01:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (8.14%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public: 
+    int Count(long curr,long next , int n){
+        int CountSum=0;
+        while(curr <= n){
+            CountSum+=(next-curr);
+            curr*=10;
+            next*=10;
+            next=min(next,(long)(n+1));
+        }
+        return CountSum;
+
+
+
+
+    }
+
+    int findKthNumber(int n, int k) {
+        int curr=1;
+        k-=1;
+        while(k>0){
+            int count=Count(curr,curr+1,n);
+            if(count <= k){
+                curr+=1;
+                k-=count;
+            }else{
+                curr*=10;
+                k-=1;
+            }
+        }
+        return curr;
+
+
+        
+    }
+};
